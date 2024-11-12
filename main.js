fd0606e8f82852ecefd21efc9dea3613ec6e6974
suhail7701 <ahamedsuhail7701@gmail.com>
2024-11-12 23:09:55 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,33 @@ app.get('/',(req,res)=>{
     res.json({msg:"Hello world!!"});
 });
 
+// CURD Functionalities
+
+// R -  For Reading
+
+app.get("/movies",()=>{
+
+});
+
+// C - For Creating movies
+
+app.post("/movies",()=>{
+
+});
+
+// U - For updating movie
+
+app.put("/movies:id",()=>{
+
+});
+
+// D - For Deleting Movie
+
+app.delete("/movies:id",()=>{
+
+});
+
+
 app.listen(PORT,()=>{
 
     console.log(`The server is Running at : http://localhost:${PORT}`);
